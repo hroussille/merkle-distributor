be52fae7ac948cba477449fef02648ec4ff8cbc6
Hector Roussille <hector.roussille@gmail.com>
2026-04-13 12:58:30 +0200

    init: contracts, interfaces and foundry configuration

---
+++ contracts/test/MintableERC20.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MintableERC20 is ERC20, Ownable {
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC20(name, symbol) {}
+
+    /**
+     * @dev Mints tokens to a specified address
+     * @param to The address to mint tokens to
+     * @param amount The amount of tokens to mint
+     */
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
